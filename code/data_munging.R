af8b2dde680bcf9d9254729319b0dec36a0a7055
Eric Hare <ericrhare@gmail.com>
2015-08-27 16:43:50 -0500

    Stop using text munging

--- code/data_munging.R
+++ code/data_munging.R
@@ -1,29 +1,35 @@
 ## Necessary libraries
 library(plyr)
 
-# Calculate the mean value for all metrics at once
-enumerateMetrics <- function(...) {
-    return(paste(unlist(lapply(c(...), function(met){return(paste(met, " = mean(", met, ", na.rm = TRUE", ")", sep = ""))})), collapse = ", "))
+## Function to compute statistics for a given set of metrics
+enumerateMetrics <- function(vars) {
+    var_names <- lapply(vars, as.name)
+    
+    exprs <- lapply(var_names, function(var) substitute(mean(var, na.rm = TRUE), list(var = var)))
+    names(exprs) <- vars
+    
+    exprs
 }
 
 ## Read in the data
 clean.all <- read.csv("../data/sotc.csv")
 comm.facts <- read.csv("../data/CommunityFacts.csv")
 latlons <- read.csv("../data/city_latlon.csv")
+metrics <- read.csv("../data/metrics.csv")
 
-## Create data frame containing metric variable names and display names
-metrics <- data.frame(var_name = c("CCE", "AESTHETI", "SOCIAL_C", "INVOLVEM", "EDUCATIO", "SOCIAL_O", "OPENNESS", "SAFETY", "BASIC_SE", "ECONOMY", "LEADERSH"),
-                      disp_name = c("Community Attachment", "Aesthetics", "Social Capital", "Civic Involvement", "Education", "Social Offerings", "Openness", "Safety", "Basic Services", "Economy", "Leadership"))
+## Build the expression for enumerateMetrics
+expr <- enumerateMetrics(as.character(metrics$var_name))
+expr$TOTALRESP <- quote(length(GENDER))
 
-## Calculate the metrics across all years and for each individual year
-expr.all <- paste(as.expression(paste("ddply(clean.all, .(QSB, URBAN_GR), summarise, ")), enumerateMetrics(as.character(metrics$var_name)), ", TOTALRESP = length(GENDER))", sep = "")
-expr.2008 <- paste(as.expression(paste("ddply(subset(clean.all, source == \"sotc08\"), .(QSB, URBAN_GR), summarise, ")), enumerateMetrics(as.character(metrics$var_name)), ", TOTALRESP = length(GENDER))", sep = "")
-expr.2009 <- paste(as.expression(paste("ddply(subset(clean.all, source == \"sotc09\"), .(QSB, URBAN_GR), summarise, ")), enumerateMetrics(as.character(metrics$var_name)), ", TOTALRESP = length(GENDER))", sep = "")
-expr.2010 <- paste(as.expression(paste("ddply(subset(clean.all, source == \"sotc10\"), .(QSB, URBAN_GR), summarise, ")), enumerateMetrics(as.character(metrics$var_name)), ", TOTALRESP = length(GENDER))", sep = "")
-clean.all.city <- eval(parse(text = expr.all))
-clean.2008.city <- eval(parse(text = expr.2008))
-clean.2009.city <- eval(parse(text = expr.2009))
-clean.2010.city <- eval(parse(text = expr.2010))
+## Set the arguments and build the data frame for ALL, 2008, 2009, and 2010
+base_args <- list(quote(clean.all), quote(.(QSB, URBAN_GR)), quote(summarise))
+clean.all.city <- eval(as.call(c(quote(ddply), base_args, expr)))
+base_args <- list(quote(subset(clean.all, source == "sotc08")), quote(.(QSB, URBAN_GR)), quote(summarise))
+clean.2008.city <- eval(as.call(c(quote(ddply), base_args, expr)))
+base_args <- list(quote(subset(clean.all, source == "sotc09")), quote(.(QSB, URBAN_GR)), quote(summarise))
+clean.2009.city <- eval(as.call(c(quote(ddply), base_args, expr)))
+base_args <- list(quote(subset(clean.all, source == "sotc10")), quote(.(QSB, URBAN_GR)), quote(summarise))
+clean.2010.city <- eval(as.call(c(quote(ddply), base_args, expr)))
 
 ## Merge the community facts and the SOTC data
 comm.facts.merge <- merge(comm.facts, latlons, by.x = "Community", by.y = "QSB")
@@ -32,4 +38,5 @@ clean.2008.city.merge <- merge(clean.2008.city, comm.facts.merge, by.x = "QSB", 
 clean.2009.city.merge <- merge(clean.2009.city, comm.facts.merge, by.x = "QSB", by.y = "Community")
 clean.2010.city.merge <- merge(clean.2010.city, comm.facts.merge, by.x = "QSB", by.y = "Community")
 
+## Save the resulting data that will be used into an RData file
 save(metrics, clean.all, comm.facts, clean.all.city.merge, clean.2008.city.merge, clean.2009.city.merge, clean.2010.city.merge, file = "../data/clean_sotc.RData")
